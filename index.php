d137754ca7ce6edd98dbc12eb10903adb55aa1e4
Marti-code <marti.huza@gmail.com>
2025-10-13 08:29:55 +0200

    mvc base

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo "<h1>Hello students</h1>";
+# ten plik to front  controller
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::run($path);
+
+// var_dump($path);
+
+# kod odpowiedzi HTTP
+# 100 - informacyjne
+# 200 - sukces
+# 300 - przekierowania
+# 400 - błędy klienta
+# 500 - błędy serwera
 
 ?>
